41685b83beedf9fc5a75c5060ac5f0035ebd0c68
vcka <vcka@ministras.lt>
2019-01-31 21:53:30 +0200

    Generic 3

---
+++ Generics 3/src/com/company/Mapas.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Mapas <A> {
+    List<Mapas> sarasasPoru;
+
+
+    public Mapas(List<Mapas> sarasasPoru) {
+        this.sarasasPoru = sarasasPoru;
+    }
+
+public <K, V> void ideti(p2){
+        sarasasPoru.add(k,v);
+}
+
+
+}
