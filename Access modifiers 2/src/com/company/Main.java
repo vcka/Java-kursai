5e79817bb1280808a994189a9ed2ab810e5e5082
vcka <vcka@ministras.lt>
2019-02-07 20:04:09 +0200

    Maven

--- Access modifiers 2/src/com/company/Main.java
+++ Access modifiers 2/src/com/company/Main.java
@@ -5,6 +5,9 @@ import java.util.stream.IntStream;
 public class Main {
 
     public static void main(String[] args) {
+        String s = "1234567891";
+        if (s.matches("\\d{9}")) System.out.println(s);
+
         PvmValidatorius pvmValidatorius = new PvmValidatorius();
         IntStream code = pvmValidatorius.enterCode();
         pvmValidatorius.tikrintiPVMkoda(code);
---
+++ Maven 2/src/main/java/penkisimtai/Main.java
@@ -0,0 +1,31 @@
+package penkisimtai;
+
+import org.ajbrown.namemachine.Gender;
+import org.ajbrown.namemachine.Name;
+import org.ajbrown.namemachine.NameGenerator;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0) {
+            Process p = Runtime.getRuntime().exec("cmd.exe /c start java -jar " + (new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath())).getAbsolutePath() + " cmd");
+        } else {
+            //code to be executed
+        }
+        NameGenerator generator = new NameGenerator();
+
+        // generate 1000 female names
+        List<Name> names = generator.generateNames( 1000, Gender.FEMALE );
+
+
+        // generate 5000 male and female names.
+        names.forEach(System.out::println);
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+    }
+}
