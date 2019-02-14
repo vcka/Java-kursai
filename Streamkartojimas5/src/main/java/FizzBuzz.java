fe61ae608bfc68a8c535d8dcb1e98c6291969328
vcka <vcka@ministras.lt>
2019-02-14 22:52:37 +0200

    StreamKartojimas 5-Final

--- Streamkartojimas5/src/main/java/FizzBuzz.java
+++ Streamkartojimas5/src/main/java/FizzBuzz.java
@@ -1,11 +1,27 @@
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.toList;
+
 public class FizzBuzz {
-    public String fizzBuzz(Integer i) {
+    public List<Object> fizzBuzz(Integer i) {
         return IntStream.rangeClosed(1, i)
                 .mapToObj(Integer::valueOf)
-                .map(integer -> (integer % 3 == 0 ) ? "Fizz" : integer.toString())
-                .collect(Collectors.joining(","));
+                .map(integer -> {
+                    if (integer % 3 == 0 && integer % 5 == 0) {
+                        return "FizzBuzz";
+                    } else {
+                        if (integer % 3 == 0) {
+                            return "Fizz";
+                        } else {
+                            if (integer % 5 == 0) {
+                                return "Buzz";
+                            }
+                        }
+                    }
+                    return integer;
+                })
+                .collect(toList());
     }
 }
--- Streamkartojimas5/src/test/java/FizzBuzzTest.java
+++ Streamkartojimas5/src/test/java/FizzBuzzTest.java
@@ -1,11 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest extends FizzBuzz {
 
     @Test
     void fizzBuzzTest() {
-        System.out.println(fizzBuzz(35));
+        final List<Object> objects = fizzBuzz(15);
+        assertEquals("Fizz", objects.get(2));
+        assertEquals("FizzBuzz", objects.get(14));
     }
 }
